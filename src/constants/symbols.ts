bdf4048fc8cfb581f74aecf01679c9d317f7754f
¨Magali <130312385+galima10@users.noreply.github.com>
2026-08-04 20:44:48 +0200

    feat(rewards): :sparkles: Defining the payouts table

--- src/constants/symbols.ts
+++ src/constants/symbols.ts
@@ -37,3 +37,45 @@ export const reel3Map = {
   seven: 1,
   diamond: 1,
 };
+
+export const payouts = {
+  "cherry,cherry,cherry": {
+    coins: 5,
+    models: 3,
+  },
+
+  "lemon,lemon,lemon": {
+    coins: 10,
+    models: 5,
+  },
+
+  "star,star,star": {
+    coins: 25,
+    models: 10,
+  },
+
+  "seven,seven,seven": {
+    coins: 100,
+    models: 15,
+  },
+
+  "cherry,cherry,diamond": {
+    coins: 15,
+    models: 8,
+  },
+
+  "lemon,lemon,diamond": {
+    coins: 30,
+    models: 12,
+  },
+
+  "star,star,diamond": {
+    coins: 75,
+    models: 20,
+  },
+
+  "seven,seven,diamond": {
+    coins: 300,
+    models: 30,
+  },
+};
