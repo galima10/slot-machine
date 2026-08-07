a55a0c58326f1f94d991ba631d55e240b293d1ae
¨Magali <130312385+galima10@users.noreply.github.com>
2026-08-07 13:12:45 +0200

    build: :rocket: Create deploy branch

--- src/HUD/elements/hud.controls.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function HudControls(){
-    return (
-        <div className="controls">
-            <p>Déplacer la caméra : Clic droit</p>
-            <p>Intéragir : Clic gauche</p>
-        </div>
-    );
-};
--- src/SlotMachine/types/Coin.d.ts
+++
@@ -1,9 +0,0 @@
-export type CoinGLTFResult = GLTF & {
-  nodes: {
-    Coin: THREE.Mesh;
-  };
-  materials: {
-    Material: THREE.MeshStandardMaterial;
-  };
-  animations: THREE.AnimationClip[];
-};
